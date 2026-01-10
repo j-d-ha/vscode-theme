05e704ddb266c05c9f28ed2ddc667ee92adb44fe
Jonas Ha <jonas-ha@outlook.com>
2026-01-10 15:13:58 -0500

    fix(annotators): handle conditional preprocessing directives for C# keywords
    Updated `CSharpAnnotator` to exclude preprocessing directives (`PP_`) like `#if` and `#else` from syntax highlighting to prevent incorrect keyword annotation.

--- src/main/kotlin/com/github/dinbtechit/vscodetheme/annotators/CSharpAnnotator.kt
+++ src/main/kotlin/com/github/dinbtechit/vscodetheme/annotators/CSharpAnnotator.kt
@@ -3,6 +3,7 @@ package com.github.dinbtechit.vscodetheme.annotators
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.elementType
 import com.intellij.util.ObjectUtils
 
 class CSharpAnnotator : BaseAnnotator() {
@@ -19,12 +20,17 @@ class CSharpAnnotator : BaseAnnotator() {
         var type: TextAttributesKey? = null
         when (element.text) {
             "return", "yield" -> type = SECONDARY_KEYWORD
-            "if", "else", "switch", "case", "default", "break", "continue" -> type = SECONDARY_KEYWORD
+            "switch", "case", "break", "continue" -> type = SECONDARY_KEYWORD
             "try", "catch", "finally", "throw" -> type = SECONDARY_KEYWORD
             "for", "while", "do", "foreach" -> type = SECONDARY_KEYWORD
+            "if", "else", "default" -> {
+                if (element.elementType.toString().startsWith("PP_")) return null
+                type = SECONDARY_KEYWORD
+            }
             else -> {}
         }
         return type
     }
+
 
 }
