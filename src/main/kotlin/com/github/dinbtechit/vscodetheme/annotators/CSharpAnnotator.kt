49257a6feb85f50ddae03af9613b2aa8b0821a8c
Jonas Ha <jonas-ha@outlook.com>
2026-01-10 20:22:07 -0500

    refactor(annotators): simplify and improve C# keyword annotation logic
    Streamlined `CSharpAnnotator` keyword detection by consolidating cases and introducing `TokenSet` for preprocessing conditional directives, ensuring cleaner and more maintainable code.

--- src/main/kotlin/com/github/dinbtechit/vscodetheme/annotators/CSharpAnnotator.kt
+++ src/main/kotlin/com/github/dinbtechit/vscodetheme/annotators/CSharpAnnotator.kt
@@ -3,34 +3,42 @@ package com.github.dinbtechit.vscodetheme.annotators
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.elementType
+import com.intellij.psi.tree.TokenSet
 import com.intellij.util.ObjectUtils
+import com.jetbrains.rider.languages.fileTypes.csharp.kotoparser.lexer.CSharpTokenType
 
 class CSharpAnnotator : BaseAnnotator() {
     companion object {
         val DEFAULT_KEYWORD: TextAttributesKey = ObjectUtils.notNull(
-            TextAttributesKey.find("DEFAULT_KEYWORD"), DefaultLanguageHighlighterColors.KEYWORD
+            TextAttributesKey.find("DEFAULT_KEYWORD"),
+            DefaultLanguageHighlighterColors.KEYWORD
         )
+
         val SECONDARY_KEYWORD: TextAttributesKey = TextAttributesKey.createTextAttributesKey(
             "DEFAULT_SECONDARY_KEYWORD",
             DEFAULT_KEYWORD
         )
+
+        private val PREPROCESSOR_CONDITIONALS = TokenSet.create(
+            CSharpTokenType.PP_IF_SECTION,
+            CSharpTokenType.PP_ELSE_SECTION,
+            CSharpTokenType.PP_DEFAULT,
+        )
     }
+
     override fun getKeywordType(element: PsiElement): TextAttributesKey? {
-        var type: TextAttributesKey? = null
-        when (element.text) {
-            "return", "yield" -> type = SECONDARY_KEYWORD
-            "switch", "case", "break", "continue" -> type = SECONDARY_KEYWORD
-            "try", "catch", "finally", "throw" -> type = SECONDARY_KEYWORD
-            "for", "while", "do", "foreach" -> type = SECONDARY_KEYWORD
+        return when (element.text) {
+            "return", "yield",
+            "switch", "case", "break", "continue",
+            "try", "catch", "finally", "throw",
+            "for", "while", "do", "foreach" -> SECONDARY_KEYWORD
+
             "if", "else", "default" -> {
-                if (element.elementType.toString().startsWith("PP_")) return null
-                type = SECONDARY_KEYWORD
+                if (PREPROCESSOR_CONDITIONALS.contains(element.node.elementType)) null
+                else SECONDARY_KEYWORD
             }
-            else -> {}
+
+            else -> null
         }
-        return type
     }
-
-
 }
